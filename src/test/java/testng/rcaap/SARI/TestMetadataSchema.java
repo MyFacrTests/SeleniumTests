512acd3aed1dbb4ef3924df1890291ca758d0b56
Fernando <facr73@yahoo.com>
2016-10-30 13:11:57 +0000

    Changed test for metadata because some dropdown were not seen in selenium and the alternatives gave some other links. These testes need to be confirmed also in headless

--- src/test/java/testng/rcaap/SARI/TestMetadataSchema.java
+++ src/test/java/testng/rcaap/SARI/TestMetadataSchema.java
@@ -9,14 +9,20 @@ import static org.testng.Assert.*;
 public class TestMetadataSchema extends Login {
 
 	
-	@Test(dataProvider = "plumx", dataProviderClass = DataProviderClass.class)
+	@Test(dataProvider = "urls-all", dataProviderClass = DataProviderClass.class)
 	public void testMetadataSchema(String baseUrl) throws Exception {
-		 driver.get(baseUrl + "//dspace-admin/metadata-schema-registry");
+		 driver.get(baseUrl + "/password-login");
 		    System.out.println("user: " + user + "pass: " + pass);
 		    driver.findElement(By.id("tlogin_email")).sendKeys(user);
 		    driver.findElement(By.id("tlogin_password")).clear();
 		    driver.findElement(By.id("tlogin_password")).sendKeys(pass);
 		    driver.findElement(By.name("login_submit")).click();
+		    /*Thread.sleep(100);
+		    driver.get(baseUrl + "/dspace-admin/metadata-schema-registry");*/
+		    driver.findElement(By.xpath("//a[contains(text(), '" + user +"')]")).click();
+		    driver.get(baseUrl + "/dspace-admin");
+		    driver.findElement(By.xpath("(//a[contains(@href, '#')])[4]")).click();
+		    driver.findElement(By.xpath("//a[contains(@href, '/dspace-admin/metadata-schema-registry')]")).click();
 	    try {
 	    	 assertTrue(isElementPresent(By.xpath("//main[@id='content']/div[3]/form/div[3]/input")));
 	    	 driver.get(baseUrl + "/logout");
